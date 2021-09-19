449fe7adf97302bb5cb4e4dd0bca5d0ea8ab9006
leandro47 <leandrosilva47@live.com>
2021-09-19 11:33:08 -0300

    tests

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,21 +2,47 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
-    private float $maiorValor;
+    private float $maiorValor = -INF;
+    private float $menorValor = INF;
+    private array $maioresLances = [];
 
     public function avalia(Leilao $leilao): void
     {
+        foreach ($leilao->getLances() as $lance) {
+            if ($lance->getValor() > $this->maiorValor) {
+                $this->maiorValor = $lance->getValor();
+            }
+
+            if ($lance->getValor() < $this->menorValor) {
+                $this->menorValor = $lance->getValor();
+            }
+        }
+        
         $lances = $leilao->getLances();
-        $utlimoLance = $lances[count($lances) -1];
-        $this->maiorValor = $utlimoLance->getValor();
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+        
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
     
     public function getMaiorValor(): float
     {
         return $this->maiorValor;
+    }
+    
+    public function getMenorValor(): float
+    {
+        return $this->menorValor;
+    }
+    
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
--- teste-avaliador.php
+++ teste-avaliador.php
@@ -7,17 +7,27 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+// Arrange - Given
 $leilao = new Leilao('Fiat 147 0KM');
 
 $maria = new Usuario('Maria');
-$joao = new Usuario('Joao');
+$joao = new Usuario('João');
 
 $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+// Act - When
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
 
-echo $maiorValor . PHP_EOL;
+// Assert - Then
+$valorEsperado = 2500;
+
+if ($valorEsperado == $maiorValor) {
+    echo "TESTE OK";
+} else {
+    echo "TESTE FALHOU";
+}
